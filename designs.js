7c527e5497882e42cf3f539e5406a9a1ed518266
Franco Alvarado <franco@Francos-MacBook-Pro.local>
2018-01-29 19:37:55 -0500

    started design js
    design js

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
+var height = $('#inputHeight');
+var width = $('#inputWidth');
+var table = $('#pixelCanvas');
+$(document).ready(function() {
 
 function makeGrid() {
-
-// Your code goes here!
-
+	var h = Number(height.val());
+	var w = Number(width.val());
+	table.prepend('<tr><td></td></tr>');
+	console.log((w + h) + table);
+	// for (var i = 0; i < w; i++) {
+	// 	table.prepend("<tr></tr>");
+	// 	for (var j = 0; j < h; j++) {
+	// 		table.children()[0].append('<td></td>');
+	// 	}
+		
+	// }
+  	
 }
+$('input[type="submit"]').click(makeGrid);
+  
+  
+});
